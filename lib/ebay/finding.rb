50319a75b72abe5005500e2ddd025d5da3b38fa8
Mark Hoffman <markautorevo@gmail.com>
2011-09-09 16:54:02 -0500

    Refactored Finding to DRY up some of the URL building and to ensure that the app_id is picked up properly.

--- lib/ebay/finding.rb
+++ lib/ebay/finding.rb
@@ -5,18 +5,24 @@ module Ebay
     SERVICE_VERSION="1.0.0"
     
     include HTTParty
-    base_uri "svcs.ebay.com/services/search/FindingService/v1?SERVICE-VERSION=#{SERVICE_VERSION}&SECURITY-APPNAME=#{Ebay::Api.app_id}&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD"
+    base_uri "svcs.ebay.com/services/search/FindingService/v1?SERVICE-VERSION=#{SERVICE_VERSION}&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD"
     format :json
 
-    def self.find_by_keyword(keywords,page_number)
-      parse_response(get("&OPERATION-NAME=findItemsByKeywords&paginationInput.pageNumber=#{page_number}",:query=>{:keywords=>keywords}))
+    def self.find_by_keyword(params)
+      params.merge!({:call_name=>"findItemsByKeywords"})
+      parse_response(get(Finding.build_call(params),:query=>{:keywords=>params[:keywords]}))
     end
     
-    def self.find_by_category(category,page_number)
-      parse_response(get("&OPERATION-NAME=findItemsByCategory&paginationInput.pageNumber=#{page_number}",:query=>{:categoryId=>category}))
+    def self.find_by_category(params)
+      params.merge!({:call_name=>"findItemsByCategory"})
+      parse_response(get(Finding.build_call(params),:query=>{:categoryId=>params[:category]}))
     end
     
     def self.parse_response(response)
+      if !response.parsed_response.has_key?("findItemsByCategoryResponse")
+        return {:ack=>:failure,:message=>response["errorMessage"][0]["error"][0]["message"][0],:items=>nil,:total_pages=>nil,:page_number=>nil}
+      end
+      
       response=response.parsed_response["findItemsByCategoryResponse"][0]
       ack=response["ack"][0].downcase.to_sym
       message=response["errorMessage"][0]["error"][0]["message"][0] if ack==:failure
@@ -26,6 +32,10 @@ module Ebay
         page_number=Integer(response["paginationOutput"][0]["pageNumber"].first)
       end
       {:ack=>ack,:message=>message,:items=>items,:total_pages=>total_pages,:page_number=>page_number}
+    end
+    
+    def self.build_call(params)
+      "&SECURITY-APPNAME=#{Ebay::Api.app_id}&GLOBAL-ID=#{params[:global_id]}&OPERATION-NAME=#{params[:call_name]}&paginationInput.pageNumber=#{params[:page_number]}"
     end
   end
 end
