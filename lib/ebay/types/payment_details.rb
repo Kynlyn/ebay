83a5b6b8a6fcfa5a4258e7994f6144fe2f3177a0
Mark Hoffman <markautorevo@gmail.com>
2012-03-19 15:49:43 -0500

    Added DepositType to PaymentDetails

--- lib/ebay/types/payment_details.rb
+++ lib/ebay/types/payment_details.rb
@@ -11,6 +11,7 @@ module Ebay # :nodoc:
       numeric_node :hours_to_deposit, 'HoursToDeposit', :optional => true
       numeric_node :days_to_full_payment, 'DaysToFullPayment', :optional => true
       numeric_node :deposit_amount,"DepositAmount",:optional => true
+      text_node :deposit_type,"DepositType",:optional => true
     end
   end
 end
